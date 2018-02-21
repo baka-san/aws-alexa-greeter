3678d6b37214d7c8c4f6d80b71297079c6087487
Grant <gsbackes@gmail.com>
2018-02-21 16:28:23 +0900

    Greeter skill using a web service as an endpoint

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# aws-alexa-greeter
+# AWS Alexa Greeter
+
+### Section 1: Greeting using an AWS Lambda function  
+#### **Code found at:** 
+
+**Description**  
+A skill called Greeter was created on [developer.amazon](https://developer.amazon.com/). The function code was executed using a Lambda function found on [console.aws.amazon](console.aws.amazon.com). The code was written with Node.js and tested using the Mocha test framework.
+
+The code can be invoked with phrases such as: 
+
+
+> Ask Greeter to say hello to John.  
+> Ask Greeter to greet our guest John.  
+> Ask Greeter to greet John.
+
+This will return a response like:
+
+> Hello John, good morning. That's spelt J-O-H-N, isn't it? Here's a nice quote for you: *Zombies eat brains so you're safe.*
+
+You can get another quote with phrases such as:
+> Ask Greeter to get me a quote.  
+> Ask Greeter to get a quote.
+
+Greeter will ask you if you'd like to hear another quote. You can respond with phrases such as:
+
+> More  
+> One more  
+> Yes  
+> No
+> Stop
+
+Both simple and standard [cards](https://developer.amazon.com/docs/custom-skills/include-a-card-in-your-skills-response.html) (i.e. has an image) are available to be viewed on appropriate devices.
--- webservice/greeter.py
+++ webservice/greeter.py
@@ -14,8 +14,8 @@ def hello():
 def alexa():
   event = request.get_json()
 
-  print('\nREQUEST:\n')
-  print(event)
+  print '\nREQUEST:\n'
+  print json.dumps(event, indent=2, sort_keys=True)
 
   req = event['request']
 
@@ -24,6 +24,11 @@ def alexa():
 
   elif req['type'] == 'IntentRequest':
     if req['intent']['name'] == 'HelloIntent':
+      # print(handle_hello_intent(req))
+      # print("start")
+      # print(message['response'])
+      # print("end")
+
       return handle_hello_intent(req)
     # elif req['intent']['name'] == 'QuoteIntent':
     #   return handle_quote_intent()
@@ -41,12 +46,12 @@ def handle_hello_intent(req):
   """Handles hello intent and generates a response"""
 
   name = req['intent']['slots']['FirstName']['value']
-  response = Response()
-  response.speechText = "Hello {0} . ".format(name)
-  response.speechText += "That\'s spelt as <say-as interpret-as='spell-out'>{0}</say-as>, isn\'t it?".format(name)
-  response.speechText += get_greeting()
+  res = Response()
+  res.speech_text = "Hello {0} .".format(name)
+  res.speech_text += " That\'s spelt as <say-as interpret-as='spell-out'>{0}</say-as>, isn\'t it?".format(name)
+  res.speech_text += get_greeting()
 
-  return response.build_response()
+  return res.build_response()
 
 def get_greeting():
   """Returns the proper greeting based on what time it is"""
@@ -67,13 +72,13 @@ def get_greeting():
 
 def handle_launch_request():
   """Handles launch request and generates response"""
-  response = Response()
+  res = Response()
 
-  response.speechText = "Welcome to Greetings skill. Using this skill you can greet your guests. Who do you want to greet?"
-  response.repromptText = "For example, you can say hello to John. Who would you like to greet?"
-  response.endSession = false
+  res.speech_text = "Welcome to Greetings skill. Using this skill you can greet your guests. Who do you want to greet?"
+  res.reprompt_text = "For example, you can say hello to John. Who would you like to greet?"
+  res.end_session = False
 
-  return response.build_response()
+  return res.build_response()
 
 
 class Response(object):
@@ -87,28 +92,33 @@ class Response(object):
   def build_response(self):
     """Builds alexa response and returns"""
 
-    response = {
+    fnl_res = {
       'version': '1.0',
       'response': {
         'outputSpeech': {
           'type': 'SSML',
-          'ssml': '<speak>' + str(self.speech_text) + '</speak>'
+          'ssml': '<speak>{0}</speak>'.format(self.speech_text)
         },
         'shouldEndSession': self.end_session
       }
     }
 
     if self.reprompt_text:
-      response['response']['reprompt_text'] = {
+      fnl_res['response']['reprompt_text'] = {
         'outputSpeech': {
           'type': 'SSML',
-          'ssml': '<speak>' + str(self.reprompt_text) + '</speak>'
+          'ssml': '<speak>{0}</speak>'.format(self.reprompt_text)
         }
       }
 
-    http_response = make_response(json.dumps(response))
+    print '\nRESPONSE:\n'
+    print json.dumps(fnl_res, indent=2, sort_keys=True)
+
+    http_response = make_response(json.dumps(fnl_res))
     http_response.headers['Content-Type'] = 'application/json'
     return http_response
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
+
+
